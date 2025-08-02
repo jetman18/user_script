97a3e303146eccc778800c63f230448bb97458ca
jetman18 <nongductuan10@gmail.com>
2025-08-02 15:24:21 +0700

    fix path

--- Merger_app_bl/merger.sh
+++ Merger_app_bl/merger.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Đường dẫn firmware và bootloader
-FIRMWARE="/home/tuan/ardupilot/build/SpeedyBeeF405WING/bin/arducopter.bin"
-BOOTLOADER="/home/tuan/ardupilot/build/SpeedyBeeF405WING/bin/AP_Bootloader.bin"
+FIRMWARE="../../build/SpeedyBeeF405WING/bin/arducopter.bin"
+BOOTLOADER="../../build/SpeedyBeeF405WING/bin/AP_Bootloader.bin"
 BOOTLOADER_SIZE_KB=15
 
 # Kiểm tra file tồn tại
@@ -19,6 +19,6 @@ if [[ ! -f "$BOOTLOADER" ]]; then
   exit 1
 fi
 
-python3 /home/tuan/ardupilot/Tools/scripts/make_intel_hex.py "$FIRMWARE" "$BOOTLOADER" "$BOOTLOADER_SIZE_KB"
+python3 ../../Tools/scripts/make_intel_hex.py "$FIRMWARE" "$BOOTLOADER" "$BOOTLOADER_SIZE_KB"
 
 echo "✅ generated firmware with bootloader."
